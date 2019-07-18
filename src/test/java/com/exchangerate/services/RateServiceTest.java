9c114f2f85dd7ae72a4b590403fa4955fa55ccac
sergiopoliveira <sergiopoliveira@outlook.com>
2019-07-18 15:44:14 +0100

    identation

--- src/test/java/com/exchangerate/services/RateServiceTest.java
+++ src/test/java/com/exchangerate/services/RateServiceTest.java
@@ -14,36 +14,36 @@ import static org.mockito.Mockito.when;
 
 public class RateServiceTest {
 
-	private RateService rateService;
+    private RateService rateService;
 
-	@Mock
-	private RateRepository rateRepository;
-	
-	@Before
-	public void setUp() throws Exception {
-		MockitoAnnotations.initMocks(this);
-		rateService = new RateServiceImpl(rateRepository);
-	}
-	
-	@Test
-	public void getRate() throws Exception {
-		
-		RateDTO rate1 = new RateDTO();
-		Calendar calendar = Calendar.getInstance();
-		
-		rate1.setYear(calendar.get(Calendar.YEAR));
-		rate1.setMonth(calendar.get(Calendar.MONTH) + 1);
-		rate1.setDay(calendar.get(Calendar.DAY_OF_MONTH));
-		
-		// given
-		when(rateService.getRate("2015-03-03", "EUR", "USD")).thenReturn(rate1);
-		
-		// when
-		RateDTO rateDTO = rateService.getRate("2015-03-03", "EUR", "USD");
-		
-		// then
-		assertEquals(rate1.getYear(), rateDTO.getYear());
-		assertEquals(rate1.getMonth(), rateDTO.getMonth());
-		assertEquals(rate1.getDay(), rateDTO.getDay());
-}
+    @Mock
+    private RateRepository rateRepository;
+
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+        rateService = new RateServiceImpl(rateRepository);
+    }
+
+    @Test
+    public void getRate() throws Exception {
+
+        RateDTO rate1 = new RateDTO();
+        Calendar calendar = Calendar.getInstance();
+
+        rate1.setYear(calendar.get(Calendar.YEAR));
+        rate1.setMonth(calendar.get(Calendar.MONTH) + 1);
+        rate1.setDay(calendar.get(Calendar.DAY_OF_MONTH));
+
+        // given
+        when(rateService.getRate("2015-03-03", "EUR", "USD")).thenReturn(rate1);
+
+        // when
+        RateDTO rateDTO = rateService.getRate("2015-03-03", "EUR", "USD");
+
+        // then
+        assertEquals(rate1.getYear(), rateDTO.getYear());
+        assertEquals(rate1.getMonth(), rateDTO.getMonth());
+        assertEquals(rate1.getDay(), rateDTO.getDay());
+    }
 }

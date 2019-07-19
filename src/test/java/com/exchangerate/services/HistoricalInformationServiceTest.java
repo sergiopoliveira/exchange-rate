94931a2d5135638046a72df7efc211c0575b79cd
sergiopoliveira <sergiopoliveira@outlook.com>
2019-07-19 15:13:38 +0100

    InvalidParameterException tests

--- src/test/java/com/exchangerate/services/HistoricalInformationServiceTest.java
+++ src/test/java/com/exchangerate/services/HistoricalInformationServiceTest.java
@@ -1,6 +1,7 @@
 package com.exchangerate.services;
 
 import com.exchangerate.dto.RateDTO;
+import com.exchangerate.exceptions.InvalidParameterException;
 import com.exchangerate.repositories.RateRepository;
 import org.junit.Before;
 import org.junit.Test;
@@ -8,6 +9,7 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
@@ -28,7 +30,7 @@ public class HistoricalInformationServiceTest {
     }
 
     @Test
-    public void getMonthlyRates() {
+    public void testMonthlyRates() {
 
         RateDTO rate1 = new RateDTO();
         RateDTO rate2 = new RateDTO();
@@ -56,7 +58,7 @@ public class HistoricalInformationServiceTest {
     }
 
     @Test
-    public void getDailyyRates() throws Exception {
+    public void testDailyRates() {
 
         RateDTO rate1 = new RateDTO();
         RateDTO rate2 = new RateDTO();
@@ -85,4 +87,44 @@ public class HistoricalInformationServiceTest {
         // then
         assertEquals(3, rateDTOs.size());
     }
+
+    @Test(expected = InvalidParameterException.class)
+    public void testIncorrectlyParsedDateForDailyRate() {
+
+        RateDTO rate1 = new RateDTO();
+        rate1.setYear(2015);
+        rate1.setMonth(14); // impossible month
+        rate1.setDay(1);
+
+        // given
+        List<RateDTO> rates = Collections.singletonList(rate1);
+
+        when(rateRepository.findAll()).thenReturn(rates);
+
+        // when
+        List<RateDTO> rateDTOs = historicalInformationService.getDaily("2015", "14", "01");
+
+        // exception is thrown
+    }
+
+    @Test(expected = InvalidParameterException.class)
+    public void testIncorrectlyParsedDateForMontlyRate() {
+
+        RateDTO rate1 = new RateDTO();
+        rate1.setYear(1990); // impossible year
+        rate1.setMonth(10);
+        rate1.setDay(1);
+
+        // given
+        List<RateDTO> rates = Collections.singletonList(rate1);
+
+        when(rateRepository.findAll()).thenReturn(rates);
+
+        // when
+        List<RateDTO> rateDTOs = historicalInformationService.getMonthly("1990", "10");
+
+        // exception is thrown
+    }
+
+
 }
